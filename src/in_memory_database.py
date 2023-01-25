dbd68ed1f598d15322bdf16811df51641162a94f
Conrad Mugabe <58117111+conradmugabe@users.noreply.github.com>
2023-01-25 16:40:32 +0300

    impleted in memory database service

--- src/in_memory_database.py
+++ src/in_memory_database.py
@@ -1,7 +1,8 @@
 """in memory database service"""
 from typing import List
+from uuid import uuid4
 
-from src.todo import Todo
+from src.todo import Todo, CreateTodo
 from src.database import TodoDataBaseService
 
 
@@ -15,8 +16,11 @@ class TodoInMemoryDatabaseService(TodoDataBaseService):
             data = []
         self._data = data
 
-    def save_todo(self):
+    def save_todo(self, data: CreateTodo):
         """saves todo to database"""
+        todo: Todo = {"id": uuid4(), **data}
+        self._data.append(todo)
+        return todo
 
     def read_all(self) -> List[Todo]:
         """retrieve todos from database"""
@@ -24,12 +28,26 @@ class TodoInMemoryDatabaseService(TodoDataBaseService):
 
     def read_by_id(self, todo_id: str):
         """retrieve a todo from the database"""
+        for todo in self._data:
+            if todo["id"] == todo_id:
+                return todo
+        return None
 
-    def update(self, todo_id: str):
+    def update(self, todo: Todo):
         """update a todo in the database"""
+        for index, value in enumerate(self._data):
+            if value["id"] == todo["id"]:
+                self._data[index] = {**value, **todo}
+                return self._data[index]
+        return None
 
     def delete(self, todo_id: str):
         """delete a todo from the database"""
+        for index, value in enumerate(self._data):
+            if value["id"] == todo_id:
+                self._data = self._data[:index] + self._data[index + 1 :]
+                return value
+        return None
 
     @staticmethod
     def get_instance(data: List[Todo] = None, re_init=False):
--- tests/test_in_memory_database.py
+++ tests/test_in_memory_database.py
@@ -1,5 +1,8 @@
+"""in memory database service test"""
+from typing import List
+
+from src.todo import Todo, CreateTodo
 from src.in_memory_database import TodoInMemoryDatabaseService
-from src.todo import Todo
 
 
 class TodoInMemoryDatabaseServiceTest:
@@ -19,16 +22,80 @@ class TodoInMemoryDatabaseServiceTest:
 
         assert instance_one != instance_two
 
-    def test_successfully_instantiates_with_todo_array(self):
+    def test_successfully_instantiates_with_todo_array(self, todo_list: List[Todo]):
         """database instances"""
-        todo_one: Todo = {
-            "id": "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
-            "title": "test title 1",
-            "user_id": "913694c6-435a-4366-ba0d-da5334a611b2",
-            "completed": False,
-        }
-        data = [todo_one]
-        database = TodoInMemoryDatabaseService.get_instance(data=data, re_init=True)
+        database = TodoInMemoryDatabaseService.get_instance(
+            data=todo_list, re_init=True
+        )
+        assert database.read_all() == todo_list
+        assert len(database.read_all()) == 1
+
+    def test_successfully_saves_todo(self, todo_data: CreateTodo):
+        """successfully saves data to database"""
+
+        database = TodoInMemoryDatabaseService.get_instance(re_init=True)
+        todo = database.save_todo(todo_data)
+
+        assert todo["completed"] == todo_data["completed"]
+        assert todo["title"] == todo_data["title"]
+        assert todo["user_id"] == todo_data["user_id"]
+        assert database.read_all() == [todo]
+        assert len(database.read_all()) == 1
+
+    def test_returns_todo_if_found_in_database(self, todo_list: List[Todo]):
+        """returns todo if in database"""
+        database = TodoInMemoryDatabaseService.get_instance(
+            data=todo_list, re_init=True
+        )
+        todo_id = todo_list[0]["id"]
+        todo = database.read_by_id(todo_id=todo_id)
+
+        assert todo == todo_list[0]
+
+    def test_returns_none_if_todo_not_found(self):
+        """returns None if todo not found"""
+        database = TodoInMemoryDatabaseService.get_instance(re_init=True)
+
+        todo = database.read_by_id(todo_id="123213")
+
+        assert todo is None
+
+    def test_successfully_deletes_todo(self, todo_list: List[Todo]):
+        """successfully deletes todo from database, given an id"""
+        database = TodoInMemoryDatabaseService.get_instance(
+            data=todo_list, re_init=True
+        )
 
         assert len(database.read_all()) == 1
-        assert database.read_all() == data
+
+        database.delete("fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a")
+
+        assert len(database.read_all()) == 0
+
+    def test_successfully_updates_todo(self, todo_list: List[Todo]):
+        """successfully updates todo"""
+        database = TodoInMemoryDatabaseService.get_instance(
+            data=todo_list, re_init=True
+        )
+        todo_id = todo_list[0]["id"]
+        todo_before_update = database.read_by_id(todo_id=todo_id)
+
+        todo: Todo = {**todo_before_update, "completed": True}
+
+        todo_after_update = database.update(todo=todo)
+
+        assert not todo_before_update["completed"]
+        assert todo_after_update["completed"]
+        assert todo_before_update["completed"] != todo_after_update["completed"]
+        assert todo_before_update["id"] == todo_after_update["id"]
+        assert todo_before_update["user_id"] == todo_after_update["user_id"]
+        assert todo_before_update["title"] == todo_after_update["title"]
+
+    def test_returns_none_if_no_update_occurred(self, todo_list: List[Todo]):
+        """returns none if no update occurs"""
+        database = TodoInMemoryDatabaseService.get_instance(re_init=True)
+
+        todo = todo_list[0]
+        updated_todo = database.update(todo=todo)
+
+        assert updated_todo is None
