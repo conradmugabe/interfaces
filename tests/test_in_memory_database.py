5f6951431a9a725400f6e241539ca6bee23ebba2
Conrad Mugabe <58117111+conradmugabe@users.noreply.github.com>
2023-01-25 13:09:33 +0300

    added singleton method to in memory database class

--- tests/test_in_memory_database.py
+++ tests/test_in_memory_database.py
@@ -4,6 +4,16 @@ from src.in_memory_database import TodoInMemoryDatabaseService
 class TodoInMemoryDatabaseServiceTest:
     """TodoInMemoryDatabaseService Test class"""
 
-    def test_successfully_retrieves_todos(self):
-        """"""
-        assert True
+    def test_uses_same_instance_of_the_database(self):
+        """database instances"""
+        instance_one = TodoInMemoryDatabaseService.get_instance()
+        instance_two = TodoInMemoryDatabaseService.get_instance()
+
+        assert instance_one == instance_two
+
+    def test_uses_different_instances_of_the_database(self):
+        """database instances"""
+        instance_one = TodoInMemoryDatabaseService.get_instance(re_init=True)
+        instance_two = TodoInMemoryDatabaseService.get_instance(re_init=True)
+
+        assert instance_one != instance_two
