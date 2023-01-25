07e7f3009b3be5d924fa2f6441cae602f04d4cbb
Conrad Mugabe <58117111+conradmugabe@users.noreply.github.com>
2023-01-25 11:45:19 +0300

    added a generic passing test

--- tests/in_memory_database.py
+++ tests/in_memory_database.py
@@ -3,3 +3,7 @@ from src.in_memory_database import TodoInMemoryDatabaseService
 
 class TodoInMemoryDatabaseServiceTest:
     """TodoInMemoryDatabaseService Test class"""
+
+    def test_successfully_retrieves_todos(self):
+        """"""
+        assert True
